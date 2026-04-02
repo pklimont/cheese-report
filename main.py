4b59eff0d1c4d144ed1f45101f545767b569a025
Przemysław Klimont <przemyslaw.klimont@ttsw.com.pl>
2026-04-02 15:13:27 +0200

    Add class Product

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 
+
+class Product:
+    def __init__(self, name:str, price_per_kg:float,quantity_kg:float=1.0):
+        self.name = name
+        self.price_per_kg = price_per_kg
+        self.quantity_kg = quantity_kg
+
+        def total_price() -> float:
+            return self.price_per_kg * self.quantity_kg
+
+
+
+products = [
+    Product("Roquefort", 12.50),
+    Product("Stilton", 11.24),
+    Product("Brie", 9.30),
+    Product("Gouda", 8.55),
+    Product("Edam", 11.00),
+    Product("Parmezan", 16.50),
+    Product("Mozzarella", 14.00),
+    Product("Ser owczy", 122.32),
+]
+
+for product in products:
+    print(product.name, product.price_per_kg, product.quantity_kg)
+
+
+
